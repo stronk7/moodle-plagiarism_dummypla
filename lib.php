18e2b0f637c3a95a637167ba1911e12fba020d53
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2019-04-15 18:53:44 +0200

    Now plagiarism_dummypla hooks on after_require_login
    For every module page it shows a simple notification:
    
    after_require_login @ plagiarism_dummypla intercepted this xxxx page!

--- lib.php
+++ lib.php
@@ -4,6 +4,21 @@ require_once($CFG->dirroot.'/plagiarism/lib.php');
 
 defined('MOODLE_INTERNAL') || die;
 
+/**
+ * Now plagiarism_dummypla hooks into after_require_login
+ */
+function plagiarism_dummypla_after_require_login(
+        $courseorid, $autologinguest=true, $cm=null,
+        $setwantsurltome=true, $preventredirect=false) {
+
+    if (!empty($cm) and strpos(me(), '/mod/') !== false) {
+        $alreadynotified = true;
+        \core\notification::warning('after_require_login @ plagiarism_dummypla intercepted this ' .
+            $cm->get_module_type_name() . ' page!');
+    }
+
+}
+
 class plagiarism_plugin_dummypla extends plagiarism_plugin {
 
     public function get_links($linkarray) {
